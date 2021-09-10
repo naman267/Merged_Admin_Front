a7c9cfc0ee7303fde6db8eb032f10138e75a8c2e
Naman Jain <namanb139j@gmail.com>
2021-09-10 23:01:50 +0530

    merged admin panel and front page

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Admin from './admin/app/App'
+import Web from './REACT/app'
+import { Switch,Route, Redirect } from 'react-router-dom'
+function main() {
+    return(
+        <>
+        <Switch>
+
+        <Route exact path="/web"  component={Web}/>
+        <Route  path="/dashboard" component={Admin}/>
+        <Route  path="/basic-ui/buttons" component={ Admin } />
+    
+       
+       <Route exact path="/basic-ui/buttons" component={Admin } />
+       <Route path="/basic-ui/dropdowns" component={ Admin} />
+       <Route path="/basic-ui/typography" component={ Admin } />
+
+       <Route path="/form-Elements/basic-elements" component={Admin} />
+
+       <Route path="/tables/basic-table" component={ Admin } />
+
+       <Route path="/icons/mdi" component={ Admin } />
+
+       <Route path="/charts/chart-js" component={ Admin} />
+
+
+       <Route path="/user-pages/login-1" component={ Admin } />
+       <Route path="/user-pages/register-1" component={ Admin} />
+
+       <Route path="/error-pages/error-404" component={ Admin } />
+       <Route path="/error-pages/error-500" component={ Admin } />
+
+
+       <Redirect to="/dashboard" />
+       </Switch>
+        
+
+        </>
+    )
+}
+export default main
